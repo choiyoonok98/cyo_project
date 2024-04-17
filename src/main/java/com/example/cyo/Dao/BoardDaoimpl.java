add60f8b0c50c15b114742ee9300b6259245e6d9
danbplus <danbplus@DESKTOP-0F4TG1P>
2024-04-17 16:02:23 +0900

    240417 CYO 본사커밋

--- src/main/java/com/example/cyo/Dao/BoardDaoimpl.java
+++ src/main/java/com/example/cyo/Dao/BoardDaoimpl.java
@@ -1,5 +1,40 @@
 package com.example.cyo.Dao;
 
-public class BoardDaoimpl {
+import java.util.List;
 
+import com.example.cyo.Dto.BoardDto;
+
+public class BoardDaoimpl implements BoardDao {
+
+	@Override
+	public List<BoardDto> selectList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BoardDto selectOne(int board_seq) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insert(BoardDto dto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(BoardDto dto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(int board_seq) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
 }
