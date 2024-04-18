cdd0b3056266887275bf267908e5b1cc17d4df34
choiyoonok98 <yoonok.choi@danbplus.com>
2024-04-18 18:09:58 +0900

    240418 재택근무 커밋

--- src/main/java/com/example/cyo/controller/HomeController.java
+++ src/main/java/com/example/cyo/controller/HomeController.java
@@ -1,13 +1,32 @@
 package com.example.cyo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@Controller
 public class HomeController {
-	  @RequestMapping(value="/")
-	    public String index() {
-	        
-	        return "index";
-	    }
+	
+	/*
+	 * @RequestMapping(value="/") public String index() {
+	 * 
+	 * return "index"; }
+	 */
+	@RequestMapping(value={"/" , "/index"})
+    private ModelAndView main(HttpServletRequest request, Model model) {
+		ModelAndView mv = new ModelAndView();
+			mv.setViewName("/board/allboardList.view");
+			return mv;
+		
+		
+    }
 }
---
+++ src/main/webapp/WEB-INF/views/biz/board/allBoardList.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>allBoardListTest</title>
+</head>
+<body>
+
+</body>
+</html>
