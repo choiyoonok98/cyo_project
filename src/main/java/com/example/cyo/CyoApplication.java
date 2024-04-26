9f75383f7c4f7826bbdea0edd9063e28267d6865
choiyoonok98 <yoonok.choi@danbplus.com>
2024-04-26 16:54:13 +0900

    240426 외장톰캣용 Application.java 수정

--- src/main/java/com/example/cyo/CyoApplication.java
+++ src/main/java/com/example/cyo/CyoApplication.java
@@ -2,6 +2,8 @@ package com.example.cyo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 import lombok.extern.java.Log;
@@ -11,8 +13,13 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @ComponentScan(basePackages = "com.example")
 @SpringBootApplication
-public class CyoApplication {
+public class CyoApplication extends SpringBootServletInitializer {
+	@Override
 
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	    return builder.sources(CyoApplication.class);
+	  }
+	
 	public static void main(String[] args) {
 		log.info("cyoApplication Run");
 		SpringApplication.run(CyoApplication.class, args);
